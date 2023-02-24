3ff6857079678fbac32c9dfeb0aa7efa68f6d9ff
Nihad Valiyev <nihadev@code.edu.az>
2023-02-25 00:07:58 +0400

    task

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+
+let a = document.getElementsByClassName("number1");
+let b = document.getElementsByClassName("number2");
+let c = document.getElementsByClassName("")
+
+if (condition) {
+    
+}
